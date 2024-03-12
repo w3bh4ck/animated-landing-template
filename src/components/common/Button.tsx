141a94479d962557da3982cb1c519f9a7580fc91
w3bh4ck <amadiluckysampson@gmail.com>
2024-03-12 11:24:35 +0100

    Feat_: Add the product efficiency section

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -5,11 +5,20 @@ interface Iprops {
   label: string;
   type?: "button" | "submit" | "reset";
   bgColor?: string;
+  height?: string;
+  width?: string;
+  color?: string;
 }
 
-const Button: FC<Iprops> = ({ label, type, bgColor }) => {
+const Button: FC<Iprops> = ({ label, type, bgColor, height, width, color }) => {
   return (
-    <StyledBotton bgColor={bgColor} type={type || "button"}>
+    <StyledBotton
+      height={height}
+      width={width}
+      bgColor={bgColor}
+      type={type || "button"}
+      color={color}
+    >
       {label}
     </StyledBotton>
   );
@@ -19,13 +28,17 @@ export default Button;
 
 interface StyleProps {
   bgColor?: string;
+  height?: string;
+  width?: string;
+  color?: string;
 }
 const StyledBotton = styled.button<StyleProps>`
   border-radius: 16px;
   background-color: ${(props) => props.bgColor || props.theme.colors.white};
-  color: ${({ theme }) => theme.colors.dark};
+  color: ${(props) => (props.color ? props.color : props.theme.colors.dark)};
   padding: 0px 25px 0px 25px;
-  height: 45px;
+  height: ${(props) => props.height || "45px"};
+  width: ${(props) => props.width || "auto"};
   border: none;
   min-width: 80px;
   cursor: pointer;
